0ec1d67475c13363a4d90bb08e0a1ed03358d956
Achilles Deyeni <164770211+achiilles-deyeni@users.noreply.github.com>
2025-08-13 04:37:18 +0000

    Week 3 assignment

---
+++ python_ass3.py
@@ -0,0 +1,10 @@
+def calculate_discount(price, discount_percent):
+    """Takes the price of an item and applies a discount if the discount percent is greater than 20"""
+    if discount_percent > 20:
+        discount_amount = price * (discount_percent / 100)
+        return price - discount_amount
+    return price
+
+
+discount_percent = float(input("Enter discount percent: "))
+print(f"Amount to pay: {calculate_discount(2000, discount_percent)}")
